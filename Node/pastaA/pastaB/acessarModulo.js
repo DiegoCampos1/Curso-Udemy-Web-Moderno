1f728517c66304f495d97992f0763dd876da6b4b
Diego Matheus Campos <diegodiko05@gmail.com>
2020-05-02 20:15:18 -0300

    Mais exercicios code wars

--- Node/pastaA/pastaB/acessarModulo.js
+++ Node/pastaA/pastaB/acessarModulo.js
@@ -1,3 +1,20 @@
 //Navegando entre as pastas e importar
 const moduloA = require('../../moduloA') // No linux é camel case, então n funciona moduloa ... CUIDADO!!
 console.log(moduloA.ola) //Evitar usar o copy path.
+
+const saudacao = require('saudacao')
+
+console.log(saudacao.ola)
+
+const http = require('http')
+http.createServer((req, res) => {
+  res.write('Bom dia!')
+  res.end()
+}).listen(8080)
+
+// const http = require('http')
+
+// http.createServer((req, res) => {
+//   res.write('Bom dia!')
+//   res.end()
+// }).listen(8080)
