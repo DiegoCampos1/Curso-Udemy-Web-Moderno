7406c3de10dee1e458ea15cc0dc579df184ede98
Diego Matheus Campos <diegodiko05@gmail.com>
2020-05-02 14:20:18 -0300

    Entendendo a nagevagacao do require e o fato de ser camel case no linux

---
+++ Node/pastaA/pastaB/acessarModulo.js
@@ -0,0 +1,3 @@
+//Navegando entre as pastas e importar
+const moduloA = require('../../moduloA') // No linux é camel case, então n funciona moduloa ... CUIDADO!!
+console.log(moduloA.ola) //Evitar usar o copy path.
