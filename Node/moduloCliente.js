5963b14a337ae3dfab515b8a693a37f263f107be
Diego Campos <diegodiko05@gmail.com>
2020-05-08 12:24:31 -0300

    Atualizando arquivos da pasta Node

--- Node/moduloCliente.js
+++ Node/moduloCliente.js
@@ -1,11 +1,11 @@
 //Para importar modulos do node usar a require
 
-const moduloA = require('./modoluA');
+const moduloA = require('./moduloA');
 const moduloB = require('./moduloB');
 
 console.log(moduloA.ola)
-console.log(moduloA.bemvindo)
-console.log(moduloA.ateLogo)
+console.log(moduloA.bemVindo)
+console.log(moduloA.atelog)
 console.log(moduloA)
 
 console.log(moduloB.bomDia)
