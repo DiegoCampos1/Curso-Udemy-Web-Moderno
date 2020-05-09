eca14561efd7b64979496e98141eedcf69deb08f
Diego Campos <diegodiko05@gmail.com>
2020-05-09 17:00:38 -0300

    Resolvendo desafio dos Funcionario em Node

--- Node/funcionarios/funcionarios.js
+++ Node/funcionarios/funcionarios.js
@@ -1,7 +1,18 @@
-const url = 'http://files.cod3r.com.br/curso-js/funcionarios.json'
-const axios = require('axios')
+const url = "http://files.cod3r.com.br/curso-js/funcionarios.json";
+const axios = require("axios");
 
-axios.get(url).then(response => {
-  const funcionarios = response.data
-  console.log(funcionarios)
-})
+const chineses = (f) => f.pais === "China";
+const mulheres = (f) => f.genero === "F";
+const menorSalario = (a, b) => {
+  return a.salario < b.salario ? a : b;
+};
+
+axios.get(url).then((response) => {
+  const funcionarios = response.data;
+  const func = funcionarios
+    .filter(chineses)
+    .filter(mulheres)
+    .reduce(menorSalario);
+  console.log(func);
+});
+// Retornar mulher chinesa com menor salario genero mulher. Dica Map reduce e filter
