e4e0b60bba91551c20bea66c04afa557f7dd5f2a
Diego Campos <diegodiko05@gmail.com>
2020-06-24 14:51:20 -0300

    testes do hackerRank

--- exercicioJS/codeWars44.js
+++ exercicioJS/codeWars44.js
@@ -1,12 +1,12 @@
-function findOutlier(integers) {
-  let binary = integers.map((int, i) => Math.abs(int) % 2);
-  let count = 0;
-  for (let i = 0; i < binary.length; i++) {
-    if (binary[i] == 0) count++;
-    if (count > 1) {
-      return integers[binary.indexOf(1)];
-    } else {
-      return integers[binary.indexOf(0)];
-    }
-  }
+async function getMovieList(year) {
+  fetch(`https://jsonmock.hackerrank.com/api/movies?Year=${year}`).then(response => response.json()).then(responseJSON => data)
+  // const resp = await data.JSON();
+  // if (data.status !== 200) {
+  //     throw Error(body.detail);
+  // }
+  return data.map((movie) => movie.Title)
+  // write your code here
+  // API endpoint: https://jsonmock.hackerrank.com/api/movies?Year=<year>
 }
+
+console.log(getMovieList(2015))
